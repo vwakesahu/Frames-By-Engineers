586500b9669bc33e201451fd5376e6a74169b0ff
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-09-02 12:59:19 +0530

    Added Categories and Frames

--- web/frontend/src/pages/HomePage.jsx
+++ web/frontend/src/pages/HomePage.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import HeroSection from '../components/HeroSection'
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import Frames from "../components/Frames.jsx";
+import Pic1 from "../img/pictures/pic-1.jpg";
+import Pic2 from "../img/pictures/pic-2.jpg";
 
 const HomePage = () => {
   return (
     <div>
-        <HeroSection />
+      <HeroSection />
+      <div className="md:flex gap-8">
+        <Frames
+          innerHeight="[95%]"
+          innerWidth="[95%]"
+          outterHeight="[65%]"
+          outterHeightMobile="[95%]"
+          outterWidth="[20%]"
+          outterWidthMobile="full"
+          image={Pic1}
+        />
+        <Frames
+          innerHeight="[95%]"
+          innerWidth="[95%]"
+          outterHeight="[65%]"
+          outterHeightMobile="[95%]"
+          outterWidth="[20%]"
+          outterWidthMobile="full"
+          image={Pic2}
+        />
+        <Frames
+          innerHeight="[95%]"
+          innerWidth="[95%]"
+          outterHeight="[65%]"
+          outterHeightMobile="[95%]"
+          outterWidth="[20%]"
+          outterWidthMobile="full"
+          image={Pic1}
+        />
+        <Frames
+          innerHeight="[95%]"
+          innerWidth="[95%]"
+          outterHeight="[65%]"
+          outterHeightMobile="[95%]"
+          outterWidth="[20%]"
+          outterWidthMobile="full"
+          image={Pic1}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
