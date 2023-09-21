4463c13bbae22681efb491f52613325fd5f97923
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-09-21 12:42:33 +0530

    added frames

--- web/frontend/src/components/Frames.jsx
+++ web/frontend/src/components/Frames.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 const Frames = ({innerHeight, innerWidth, outterHeight, outterHeightMobile, outterWidth, outterWidthMobile, image}) => {
   return (
-    <div className='mt-3 md:h-screen h-420'>
-        <div className={`border-[15px] border-black md:w-${outterWidth} md:h-${outterHeight} h-${outterHeightMobile} w-${outterWidthMobile} drop-shadow-xl flex items-center justify-center`}>
-            <div className={`border-2 border-black w-${innerWidth} h-${innerHeight} } overflow-hidden flex items-center justify-center`}>
-                <img src={image} alt="wallpaper-1" className=''/>
-            </div>
+    // <div className='mt-3'>
+    //     <div className={`border-[15px] border-black md:w-${outterWidth} md:h-${outterHeight} h-${outterHeightMobile} w-${outterWidthMobile} drop-shadow-xl flex items-center justify-center`}>
+    //         <div className={`border-2 border-black w-${innerWidth} h-${innerHeight} } overflow-hidden flex items-center justify-center`}>
+    //             <img src={image} alt="wallpaper-1" className=''/>
+    //         </div>
+    //     </div>
+    // </div>
+
+
+    <div className="md:w-[20%] border-[15px] border-black p-2">
+          <img src={image} alt= "frame1" />
         </div>
-    </div>
   )
 }
 
