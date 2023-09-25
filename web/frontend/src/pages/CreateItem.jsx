05e8374d1ceb6fa40d58ef6cafdb47f21e65c3bf
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-09-25 21:20:21 +0530

    Working on create frames

--- web/frontend/src/pages/CreateItem.jsx
+++ web/frontend/src/pages/CreateItem.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import Loader from "../components/Loader";
+import { LuSubtitles } from "react-icons/lu";
+import { categories, frameColors } from "../utils/data";
 
 const CreateItem = () => {
-  return (
-    <div>CreateItem</div>
-  )
-}
+  const [title, settitle] = useState("");
+  const [category, setCategory] = useState(null);
+  const [frameColor, setFrameColor] = useState(null);
 
-export default CreateItem
+  return (
+    <div className=" ">
+      <div className=" w-full  md:w-[90%] border mt-16 p-4 font-medium md:p-12">
+        <div className=" w-full">
+          {/* <p>Add Title:</p> */}
+          <div className="w-full py-2 border-b border-gray-300 flex gap-2 ">
+            <LuSubtitles className=" text-xl text-gray-700" />
+            <input
+              type="text"
+              value={title}
+              placeholder="Give me a title..."
+              onChange={(e) => settitle(e.target.value)}
+              className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400"
+            />
+          </div>
+
+          {/* Select Category */}
+          <div className=" w-full h-full mt-6">
+            <select onChange={(e) => setCategory(e.target.value)}>
+              <option value="others" className=" bg-white w-full ">
+                Select Category
+              </option>
+              {categories &&
+                categories.map((item) => (
+                  <option key={item.id} value={item.urlParamName}>
+                    {item.name}
+                  </option>
+                ))}
+            </select>
+            <select onChange={(e) => setFrameColor(e.target.value)}>
+              <option value="others" className=" bg-white w-full ">
+                Select Category
+              </option>
+              {frameColors &&
+                frameColors.map((item) => (
+                  <option key={item.id} value={item.urlParamName}>
+                    {/* {console.log(item.color)} */}
+                    {item.color}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <select>
+            <option></option>
+          </select>
+          {/* Loader   */}
+          <div>
+            <Loader />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateItem;
