0533d7675bb378d500a80d85524f3353c8459b5f
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-09-25 21:20:02 +0530

    Added cart

--- web/frontend/src/components/Navbar.jsx
+++ web/frontend/src/components/Navbar.jsx
@@ -10,6 +10,7 @@ import { actionType } from "../context/reducer";
 import { CiLogout } from "react-icons/ci";
 
 import { IoIosAdd } from "react-icons/io";
+import { CiShoppingCart } from "react-icons/ci";
 
 const Navbar = () => {
   const [isMenu, setIsMenu] = useState(false);
@@ -73,40 +74,52 @@ const Navbar = () => {
               <Link to="/about-us">About Us</Link>
             </li>
           </ul>
-
-          {user ? (
-            <img
-              src={user.photoURL}
-              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
-              onClick={handleMenu}
-            />
-          ) : (
-            <button
-              className="text-lg rounded-lg p-3 px-6 border hover:bg-black hover:text-white font-light border-1"
-              onClick={handleLogin}
-            >
-              Register Now / Login
-            </button>
-          )}
-
-          {isMenu && (
-            <div className=" absolute right-8 top-20 bg-white border rounded-lg">
-              {user && user.email === "viveksahu1762@gmail.com" && (
-                <Link to="/create-item">
-                  <div className="p-4 rounded-t-lg flex justify-start items-center gap-3 hover:bg-gray-100 font-bold cursor-pointer" onClick={handleMenu}>
-                    <IoIosAdd /> <p>Add Item</p>
-                  </div>
-                </Link>
-              )}
-
-              <div
-                className="p-4 rounded-b-lg flex justify-start items-center gap-3 hover:bg-red-500 font-bold hover:text-white cursor-pointer"
-                onClick={logout}
-              >
-                <CiLogout /> <p>Logout</p>
+          <div>
+            <div className=" flex items-center gap-7">
+              <div className="flex flex-col items-center justify-center">
+                <div className=" bg-red-500 rounded-full relative left-4 px-2 text-sm text-white">
+                  5
+                </div>
+                <CiShoppingCart className=" text-3xl cursor-pointer" />
               </div>
+              {user ? (
+                <img
+                  src={user.photoURL}
+                  className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+                  onClick={handleMenu}
+                />
+              ) : (
+                <button
+                  className="text-lg rounded-lg p-3 px-6 border hover:bg-black hover:text-white font-light border-1"
+                  onClick={handleLogin}
+                >
+                  Register Now / Login
+                </button>
+              )}
             </div>
-          )}
+
+            {isMenu && (
+              <div className=" absolute right-8 top-20 bg-white border rounded-lg">
+                {user && user.email === "viveksahu1762@gmail.com" && (
+                  <Link to="/create-item">
+                    <div
+                      className="p-4 rounded-t-lg flex justify-start items-center gap-3 hover:bg-gray-100 font-bold cursor-pointer"
+                      onClick={handleMenu}
+                    >
+                      <IoIosAdd /> <p>Add Item</p>
+                    </div>
+                  </Link>
+                )}
+
+                <div
+                  className="p-4 rounded-b-lg flex justify-start items-center gap-3 hover:bg-red-500 font-bold hover:text-white cursor-pointer"
+                  onClick={logout}
+                >
+                  <CiLogout /> <p>Logout</p>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
@@ -120,61 +133,69 @@ const Navbar = () => {
             </p>
           </div>
         </Link>
-        {user ? (
-          <img
-            src={user.photoURL}
-            onClick={handleMenu}
-            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
-          />
-        ) : (
-          <img
-            src={MenuIcon}
-            className="w-5 relative z-50"
-            alt=""
-            onClick={handleMenu}
-          />
-        )}
-
-        {isMenu && (
-          <div className="w-50 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-16 right-8 px-4 py-3">
-            <ul className="flex flex-col gap-2  ">
-              {user && user.email === "viveksahu1762@gmail.com" && (
-                <li>
-                  <Link to="/create-item" onClick={handleMenu}>
-                    Create Item
-                  </Link>
-                </li>
-              )}
-              <li onClick={handleMenu}>
-                <Link to="/">Home</Link>
-              </li>
-              <li onClick={handleMenu}>
-                <Link to="/category">Categories</Link>
-              </li>
-              <li onClick={handleMenu}>
-                <Link to="/trendy">Trendy</Link>
-              </li>
-              <li onClick={handleMenu}>
-                <Link to="/about-us">About Us</Link>
-              </li>
-              {user ? (
-                <li
-                  className=" text-sm bg-black w-28 text-white p-3 rounded-lg  flex items-center justify-center"
-                  onClick={logout}
-                >
-                  <p>Logout</p>
-                </li>
-              ) : (
-                <li
-                  className=" text-sm bg-black w-28 text-white p-3 rounded-lg  flex items-center justify-center"
-                  onClick={handleLogin}
-                >
-                  <p>Login</p>
-                </li>
-              )}
-            </ul>
+        <div className=" flex gap-7">
+          <div className="flex flex-col items-center justify-center">
+            <div className=" bg-red-500 rounded-full relative left-4 px-1 text-[0.7rem] text-white">
+              5
+            </div>
+            <CiShoppingCart className=" text-3xl" />
           </div>
-        )}
+          {user ? (
+            <img
+              src={user.photoURL}
+              onClick={handleMenu}
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+            />
+          ) : (
+            <img
+              src={MenuIcon}
+              className="w-5 relative z-50"
+              alt=""
+              onClick={handleMenu}
+            />
+          )}
+
+          {isMenu && (
+            <div className="w-50 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-16 right-8 px-4 py-3">
+              <ul className="flex flex-col gap-2  ">
+                {user && user.email === "viveksahu1762@gmail.com" && (
+                  <li>
+                    <Link to="/create-item" onClick={handleMenu}>
+                      Create Item
+                    </Link>
+                  </li>
+                )}
+                <li onClick={handleMenu}>
+                  <Link to="/">Home</Link>
+                </li>
+                <li onClick={handleMenu}>
+                  <Link to="/category">Categories</Link>
+                </li>
+                <li onClick={handleMenu}>
+                  <Link to="/trendy">Trendy</Link>
+                </li>
+                <li onClick={handleMenu}>
+                  <Link to="/about-us">About Us</Link>
+                </li>
+                {user ? (
+                  <li
+                    className=" text-sm bg-black w-28 text-white p-3 rounded-lg  flex items-center justify-center"
+                    onClick={logout}
+                  >
+                    <p>Logout</p>
+                  </li>
+                ) : (
+                  <li
+                    className=" text-sm bg-black w-28 text-white p-3 rounded-lg  flex items-center justify-center"
+                    onClick={handleLogin}
+                  >
+                    <p>Login</p>
+                  </li>
+                )}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
