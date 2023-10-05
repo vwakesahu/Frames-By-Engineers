1cc28c88d8fec7fb4ecaf85f1c3161d63f26636d
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-10-05 10:54:02 +0530

    Done with upload function

--- web/frontend/src/pages/CategoryPage.jsx
+++ web/frontend/src/pages/CategoryPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { categories } from '../utils/data'
 
 const CategoryPage = () => {
   return (
-    <div>CategoryPage</div>
+    <div>
+      <div>
+        {categories && categories.map((item)=>(
+          <div key={item.id}>{item.name}</div>
+        )) }
+      </div>
+    </div>
   )
 }
 
